e03e3473d4e591e0a08a6cd75cd7713c0e2d701e
Hasnain-Basit <hasnainfansia@gmail.com>
2026-07-07 11:42:31 +0500

    removing MONGO URI

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const connectDB = require("./src/config/db")
 const app = require("./src/app")
 connectDB()
 app.listen(3000, () => {
-    console.log(process.env.MONGO_URI)
     console.log("Server is running on localhost:3000")
 })
